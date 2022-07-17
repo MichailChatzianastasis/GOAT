78d624174449f7b11eb2e158da5392f331dc94de
Michail Chatzianastasis <mixalisx97@gmail.com>
2022-07-17 19:46:17 +0200

    Update models.py

--- models.py
+++ models.py
@@ -98,7 +98,7 @@ class GAT(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = torch.cat([att(x, adj,self.epoch) for att in self.attentions], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, adj,self.epoch))
+        x = self.out_att(x, adj,self.epoch)
         return F.log_softmax(x, dim=1)
 
 
@@ -251,7 +251,7 @@ class GATV2(nn.Module):
         x = F.elu(self.attentions(x,edge_index))
         
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.elu(self.out_att(x, edge_index))
+        x = self.out_att(x, edge_index)
         return F.log_softmax(x, dim=1)
 
 
